d9b296d5c4ee33b60e203fc3ef02ca257f06005f
Evelyn Christine <evelyncasodre@gmail.com>
2024-08-28 16:38:27 -0300

    Delete funções/questao01.py

--- funções/questao01.py
+++
@@ -1,9 +0,0 @@
-# Defina uma função chamada velocidade_media() em um script chamado funcoes.py que recebe dois parâmetros: a distância percorrida (em metros) e o tempo
-# (em segundos) gasto.
-# Entrada; 100, 20
-# Saída: 5
-
-def velocidade_media(distancia, tempo):
-    velocidade = distancia / tempo 
-    print(velocidade)
-velocidade_media(100, 20)
